c5d4231f1bb09c148845ed54a007694b43b0a7e0
imranqazi366-hub <imranqazi366@gmail.com>
2026-04-10 01:05:42 +0530

    initial upload

---
+++ jkssb-exam-app/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{padding:20}}>
+      <h1>JKSSB Exam App</h1>
+      <p>Upload your full exam code here in App.js</p>
+    </div>
+  );
+}
